80549fc31c7683075d1c9d5ab0d43569a673391d
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2022-11-13 21:46:24 +0100

    Update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./i18n";
 import App from "./components/App";
 import { AppProvider } from "./context/AppContext/AppContext";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
-import { FirebaseAppProvider } from "reactfire"
-import { firebaseConfig } from "./utils/firebase";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-          <AppProvider>
-              <App />
-          </AppProvider>
-      </FirebaseAppProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
     </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
